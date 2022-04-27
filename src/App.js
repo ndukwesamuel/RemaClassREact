7fc24354f77a69091bd9b2d9367dbd0987adca3f
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-04-27 10:27:49 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Navbar from "./Components/Navbar";
+
+// import BlogList from './Page/BlogList';
+// import Create from './Page/Create';
+// import Home from './Page/Home';
+// import Update from './Page/Update';
+
+function App() {
+  return (
+    <>
+      <h1> Welcome to First Bank </h1>
+    </>
+  );
+}
+
+export default App;
